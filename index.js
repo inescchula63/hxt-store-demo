c6ffb46922f17e9c6127a11df8cdfe2528695c1e
sutiwat41 <sutiwat51943@gmail.com>
2021-08-07 15:36:08 +0700

    test

--- index.js
+++ index.js
@@ -9,4 +9,28 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/showbalance/:code', function(req, res) {
+   const code = req.params.code
+   res.send('code: ' + code + ' and name: pending');
+})
+.get('/testconn', function(req, res) {
+  const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
