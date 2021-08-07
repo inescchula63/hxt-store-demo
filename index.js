bcce700a6e2d393d98edd82603c4bed5a7686f7b
sutiwat41 <sutiwat51943@gmail.com>
2021-08-07 10:57:09 +0700

    v0

--- index.js
+++ index.js
@@ -3,18 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 
-// app.get('/showbalance/:code', function(req, res) {
-
-//    const code = req.params.code
-//    res.send('code: ' + code + ' and name: pending');
-// });
-
-
-app.get('/', function(req, res) {
-
-   res.send('Hello World!!! welcome to <h1> haxter eiei </h1>');
- });
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
