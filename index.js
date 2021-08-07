5cb3f99f1486e19cfac56e989912a4aa2ef3d150
sutiwat41 <sutiwat51943@gmail.com>
2021-08-07 10:51:15 +0700

    v0

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
+app.get('/showbalance/:code', function(req, res) {
+
+   //if(req.params.code %2 == 0) id = "xxx"
+   //else id = "yyy"
+   const code = req.params.code
+   res.send('code: ' + code + ' and name: pending');
+});
+
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
