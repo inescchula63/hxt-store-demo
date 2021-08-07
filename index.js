53bc426eb9249b97255d94736e081c71d72b1eed
sutiwat41 <sutiwat51943@gmail.com>
2021-08-07 15:37:33 +0700

    test

--- index.js
+++ index.js
@@ -13,24 +13,5 @@ express()
    const code = req.params.code
    res.send('code: ' + code + ' and name: pending');
 })
-.get('/testconn', function(req, res) {
-  const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
