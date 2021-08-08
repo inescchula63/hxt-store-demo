4c6cc2a52cd9f2095d04946df82ebf69d874632f
sutiwat41 <sutiwat51943@gmail.com>
2021-08-08 17:13:43 +0700

    test

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ express()
   
     
 })
-.post('/testsql_show', function(req, res) {
+.get('/testsql_show', function(req, res) {
   const { Client } = require('pg');
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
