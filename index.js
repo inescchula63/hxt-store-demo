0f7187fd52295a2167fdf434177c30bd791a7007
sutiwat41 <sutiwat51943@gmail.com>
2021-08-08 19:31:36 +0700

    test

--- index.js
+++ index.js
@@ -134,8 +134,8 @@ express()
   const code = req.body.code;
   const partno = req.body.partno;
   let quantity = parseInt(req.body.quantity);
-  if( isNaN(quantity) ) quantity = 0
-  res.send(quantity)
+  //if( isNaN(quantity) ) quantity = 0
+  //res.send(quantity)
   client.query('INSERT INTO hxtstorecap (Code, PartNo, Quantity) VALUES ($1::text, $2::text,$3::int);',[code,partno,quantity], (err, result) => {
   //client.query('select * from hxtstorecap where code = \'ALP-COM-0-0\';', (err, result) => {
    // res.send("show: "+ result+ ",type :" + typeof(result) + ",Code :" + result.rows[0].code)
